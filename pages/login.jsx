964069181b6e47f6f74557446094668d6a5c0115
Johnny-Oleg <neojin.entity@gmail.com>
2022-03-06 17:06:42 +0300

    next auth

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { getProviders, signIn } from 'next-auth/react';
 
-const Login = () => {
+const Login = ({ providers }) => {
   return (
 	<div>Login</div>
   )
 }
 
 export default Login;
+
+export async function gerServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    }
+  }
+}
